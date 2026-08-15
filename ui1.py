066743e746a5e6629df2ac050fa068e360dd82bf
Saikumar Mallarapu <saikumar.pydev@gmail.com>
2026-08-15 23:53:55 +0530

    Add registration details and job update channel
    Added registration information and job opportunity links.

--- ui1.py
+++ ui1.py
@@ -1,1 +1,30 @@
+Thank you for sharing your profile.
+
+To be considered for current and upcoming job opportunities, please
+complete your registration using the links below and ensure your
+profile is created successfully with 100% completion.
+
+🔹 Registration Link 1:  https://rb.gy/av0t8n ✅
+
+🔹 Registration Link 2: https://rebrand.ly/x2muvky ✅
+
+🔹 Registration Link 3: https://go.acciojob.com/8BYrT7 ✅
+
+ Join our WhatsApp Job Updates Channel to receive daily verified job
+opportunities, hiring updates, and career-related notifications.
+
+WhatsApp Channel Link: https://whatsapp.com/channel/0029VbD1RxR5vKA7tKfDfo0w
+
+After completing your registration, please reply to this email with a
+screenshot confirming your successful profile/ID creation for both
+registrations.
+
+Please note that only candidates who complete the registration process
+and submit the required screenshots will be considered for further
+opportunities.
+
+Regards
+
+
+
 hello world
